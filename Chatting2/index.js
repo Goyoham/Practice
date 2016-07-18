6c8d4609b6c7ea87548dff23edc8cf3012363507
Goyoham <somet2001@naver.com>
2016-07-18 23:00:49 +0900

    Chat Ver2

--- Chatting2/index.js
+++ Chatting2/index.js
@@ -3,25 +3,69 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-	//res.send('<h1>Hellow World</h1>');
-	//res.sendFile(__dirname + '/index.html');
-	res.sendfile('index.html');
-});
-
-io.on('connection', function(socket){
-	/*
-	console.log('a user connected');
-
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
-	*/
-	socket.on('client message', function(msg){
-		console.log('message: ' + msg);
-		io.emit('server message', msg);
-	});
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function(){
-	console.log('listening on *:3000');
+  console.log('listening on *:3000');
+});
+
+
+
+var userList = [];
+
+
+io.on('connection', function(socket){
+  var joinedUser = false;
+  var nickname;
+
+  // 유저 입장
+  socket.on('join', function(data){
+    if (joinedUser) { // 이미 입장 했다면 중단
+      return false; 
+    }
+
+    nickname = data;
+    userList.push(nickname);
+    socket.broadcast.emit('join', { 
+      nickname : nickname
+      ,userList : userList
+    });
+
+    socket.emit('welcome', { 
+      nickname : nickname
+      ,userList : userList
+    });
+
+    joinedUser = true;
+  });
+
+
+  // 메시지 전달
+  socket.on('msg', function(data){
+    console.log('msg: ' + data);
+    io.emit('msg', { 
+      nickname : nickname
+      ,msg : data
+    });
+  });
+
+
+  // 접속 종료
+  socket.on('disconnect', function () {
+    // 입장하지 않았다면 중단
+    if ( !joinedUser) { 
+      console.log('--- not joinedUser left'); 
+      return false;
+    }
+
+    // 접속자목록에서 제거
+    var i = userList.indexOf(nickname);
+    userList.splice(i,1);
+
+    socket.broadcast.emit('left', { 
+      nickname : nickname 
+      ,userList : userList
+    });    
+  });
 });
